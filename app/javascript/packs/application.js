aacbe85bb4b9f6f1fcf7ffdd3a6540979d6f7b1d
Haris Pandzic <haris.pandzic@klika.ba>
2021-02-16 22:18:42 +0100

    Base setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+
+Turbo.setProgressBarDelay(1)
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,3 +21,8 @@ require("channels")
 window.onload = (event) => {
   console.log('Page is is fully loaded');
 };
+
+document.addEventListener("turbo:load", function() {
+  console.log("Using turbo for this click");
+})
+
